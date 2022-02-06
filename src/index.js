96d34f25420340638474820b51dc259f225267a0
Akorsikov <akors@mail.ru>
2022-02-06 22:04:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -39,6 +39,29 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let translate = '';
+    let string10;
+    function getMorseItem(stringBit) {
+        let morseItem = '';
+        let twoBits;  
+        for (let i = 9; i >= 0; i = i - 2) {          
+          twoBits = stringBit.slice(i - 1, i + 1);          
+          switch (true) {
+            case(twoBits === '11'): morseItem = '-' + morseItem;
+            break;
+            case(twoBits === '10'): morseItem = '.' + morseItem;
+            break;
+            default : return morseItem;
+          }
+        }
+        return morseItem;
+    }
+
+    for (let i = 0; i <= expr.length - 10;  i = i + 10) {
+        string10 = expr.slice(i, i + 10);
+        translate += (string10.includes('*')) ? ' ' : MORSE_TABLE[getMorseItem(string10)];
+    }
+    return translate;
 }
 
 module.exports = {
